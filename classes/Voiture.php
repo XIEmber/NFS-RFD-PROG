0c645bd0ac33591d560362a1e4271fd9bda28269
Dylan KADANI <dylan.bassier@gmail.com>
2020-01-29 11:49:54 +0100

    Oh oui détruit moi

--- classes/Voiture.php
+++ classes/Voiture.php
@@ -3,7 +3,7 @@
 class Voiture
 {
   public $nbrRoues;
-  public $nbrPlaces;
+  private $nbrPlaces = 5;
   public $masse;
   public $vitesse = 0;
   public $couleur;
@@ -13,15 +13,28 @@ class Voiture
     $this->masse = abs($m);
     $this->couleur = $c;
   }
+
+  public function getNbrPlaces()
+  {
+    return $this->nbrPlaces;
+  }
+
+  public function setNbrPlaces($p)
+  {
+    $this->nbrPlaces = $p;
+  }
+
   public function calculerEnergieCinetique()
   {
-    0.5 * $this->masse * $this->vitesse ** 2;
+    return 0.5 * $this->masse * $this->vitesse ** 2;
   }
+
   public function afficherMessageDebile()
-{
-  echo"Prête à monter en voiture Accroche ta ceinture S'il-te-plaît, mens à ton père AAAH AHHH OUUUH AHHHH OUUUUH AHHH OUHH AHH OUU HAHAH OU AHH OUUU AH OUUAH OUUAH OUUAH";
+  {
+    echo "Je roule en caisse";
+}
+  public function __destruct()
+  {
+    echo"<h1>Je suis cassé</h1>";
   }
 }
-
-
- ?>
--- index.php
+++ index.php
@@ -2,8 +2,20 @@
 require_once './classes/Voiture.php';
 
 $voiture1 = new Voiture(1000,'rouge');
+$voiture2 = new Voiture(2000,'vert pomme');
+$voiture1->vitesse = 30;
+$voiture1->vitesse = 40;
 
+echo $voiture1->getNbrPlaces();
+
+echo $voiture1->calculerEnergieCinetique();
+echo "<br/>";
+$voiture1->vitesse= 10;
+echo $voiture1->calculerEnergieCinetique();
+
+$voiture1->masse=1000;
 
 $voiture1->afficherMessageDebile();
 
-var_dump($voiture1);
+$voiture1->calculerEnergieCinetique();
+var_dump($voiture1,$voiture2);
