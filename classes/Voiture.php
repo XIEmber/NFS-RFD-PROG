c4bb2ca04ee9021fe0396c9709efcf563598e0d6
Dylan KADANI <dylan.bassier@gmail.com>
2020-01-29 10:23:37 +0100

    non

---
+++ classes/Voiture.php
@@ -0,0 +1,17 @@
+<?php
+
+class Voiture
+{
+  public $nbrRoues;
+  public $nbrPlaces;
+  public $masse;
+  public $vitesse = 0;
+
+  public function afficherMessageDebile()
+{
+  echo"vroom vroom mothar fckar";
+  }
+}
+
+
+ ?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require_once './classes/Voiture.php';
+
+$voiture1 = new Voiture();
+
+//$voiture->masse=1000;//
+
+$voiture1->afficherMessageDebile();
+?>
